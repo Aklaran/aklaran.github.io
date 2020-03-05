11ff949012e155a26ebbd6b872bf4e51b08c358b
Aklaran <botbunk@gmail.com>
2020-03-04 23:59:56 -0500

    changed layout to be overarching, added markdown support and made a blog post woooooo

--- src/js/templates/blogTemplate.js
+++ src/js/templates/blogTemplate.js
@@ -1,22 +1,19 @@
 import React from "react"
 import { graphql } from "gatsby"
 
-export default function Template({
-  data, // this prop will be injected by the GraphQL query below.
-}) {
-  const { markdownRemark } = data // data.markdownRemark holds your post data
+import PageCard from "../components/page-card"
+
+export default function BlogTemplate({ data }) {
+  const { markdownRemark } = data
   const { frontmatter, html } = markdownRemark
+
   return (
-    <div className="blog-post-container">
-      <div className="blog-post">
-        <h1>{frontmatter.title}</h1>
-        <h2>{frontmatter.date}</h2>
+    <PageCard title={frontmatter.title} date={frontmatter.date}>
         <div
           className="blog-post-content"
           dangerouslySetInnerHTML={{ __html: html }}
         />
-      </div>
-    </div>
+    </PageCard>
   )
 }
 
