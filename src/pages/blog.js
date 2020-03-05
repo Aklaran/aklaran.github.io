50641b984c6521d67dd054bdc315b87cae81bc3f
Aklaran <botbunk@gmail.com>
2020-03-04 23:59:56 -0500

    changed layout to be overarching, added markdown support and made a blog post woooooo

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,19 +1,18 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../js/components/layout"
 import Image from "../js/components/image"
 import SEO from "../js/components/seo"
 
-const BlogPage = () => (
-  <Layout>
+const BlogIndex = () => (
+  <>
     <SEO title="Home" />
     <h1>issa blog</h1>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
+  </>
 )
 
-export default BlogPage
+export default BlogIndex
