41c44ba86ba27b2369bd0d21aac5df4b07f80031
Aklaran <botbunk@gmail.com>
2020-04-04 15:40:37 -0400

    Shows site title at index, logo otherwise

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -2,13 +2,39 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import { motion, AnimatePresence } from "framer-motion"
+import { headerLogoVariant, fadeVariant } from "../utils/motion-variants"
+
 import Logo from "../../images/svg/wolf.svg"
 
-const Header = ({ siteTitle }) => (
+const Header = ({ atHome }) => (
   <header>
-      <div className="header-left">
-        <Link to="/"><Logo height="45px" width="45px"/></Link>
-      </div>
+    <AnimatePresence>
+      {atHome &&
+        <motion.h1 
+          className="header-left"
+          key="header-text"
+          variants={fadeVariant}
+          initial="initial"
+          animate={atHome ? "enter" : "initial"}
+          exit="initial"
+          >
+          Aklaran
+        </motion.h1>
+      }
+      {!atHome &&
+        <motion.div 
+          className="header-left"
+          key="header-logo"
+          variants={fadeVariant}
+          initial="initial"
+          animate={atHome ? "initial" : "enter"}
+          exit="initial"
+          >
+          <Link to="/"><Logo height="45px" width="45px"/></Link>
+        </motion.div>
+      }
+    </AnimatePresence>
       <h2 className="header-right" style={{ margin: 0 }}>
         <Link to="/work">Work</Link>&nbsp;&nbsp;&nbsp;
         <Link to="/blog">Blog</Link>&nbsp;&nbsp;&nbsp;
