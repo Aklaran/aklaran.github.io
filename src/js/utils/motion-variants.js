413168c8eccb80d1d8a0331fe1f0f15ea0d2db20
Aklaran <botbunk@gmail.com>
2020-04-04 13:49:27 -0400

    Moves variants to util file

---
+++ src/js/utils/motion-variants.js
@@ -0,0 +1,50 @@
+export const fadeVariant = {
+  initial: {
+    opacity: 0,
+  },
+  enter: {
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      delay: 0.5,
+    },
+  }
+}
+
+export const headerVariant = {
+  initial: {
+      opacity: 0,
+  },
+  enter: {
+      opacity: 1,
+      transition: {
+      duration: 1,
+      delay: 3,
+      },
+  }
+}
+
+export const landingContainerVariant = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      duration: 2,
+      delayChildren: 1,
+      staggerChildren: 0.4
+    }
+  }
+}
+
+export const landingItemVariant = {
+  hidden: { 
+    opacity: 0, 
+    y: -30 },
+  show: { 
+    opacity: 1, 
+    y: 0,
+    transition: {
+      duration: 1
+    }
+  }
+}
