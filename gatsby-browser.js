f192a50f25640f7c33e051a2567db2cff3dae907
Aklaran <botbunk@gmail.com>
2021-04-24 19:29:28 -0700

    SSR Fixes

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,9 +4,9 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-import React from 'react'
-import Layout from './src/js/components/layout'
+import React from "react"
+import Layout from "./src/js/components/layout"
 
 export const wrapPageElement = ({ element, props }) => {
-    return <Layout {...props}>{element}</Layout>
+  return <Layout {...props}>{element}</Layout>
 }
