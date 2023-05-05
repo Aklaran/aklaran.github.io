114cb1b0dab6a212e40f3791b2335368635f6c88
aklaran <botbunk@gmail.com>
2023-05-05 10:09:15 -0700

    progress on blog grid

--- src/js/components/blog-preview.js
+++ src/js/components/blog-preview.js
@@ -2,15 +2,22 @@ import React from "react"
 import PropTypes from "prop-types"
 import styled from "styled-components"
 import { Link } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 import { COLORS } from "../utils/constants"
 
-const BlogPreview = ({ title, date, path, children }) => {
+const BlogPreview = ({ title, date, path, children, image }) => {
+  const imageData = getImage(image?.childImageSharp?.gatsbyImageData)
+
   return (
     <Wrapper>
       <Link to={path} style={{ textDecoration: "none" }}>
+        <ImageWrapper>
+          <GatsbyImage image={imageData} style={{ position: "revert" }} />
+        </ImageWrapper>
         <InnerWrapper>
-          <h2>{title}</h2>
+          <Title>{title}</Title>
+          <DateDisplay>{date}</DateDisplay>
           <PreviewBody>{children}</PreviewBody>
           <More>Read more</More>
         </InnerWrapper>
@@ -26,17 +33,42 @@ BlogPreview.propTypes = {
 }
 
 const Wrapper = styled.div`
-  max-width: 600px;
-  margin: 100px auto;
+  height: 100%;
+  overflow: hidden;
+  position: relative;
+
+  background-color: white;
+  border-radius: 12px;
+
+  padding: 0;
+`
+
+const ImageWrapper = styled.div`
+  opacity: 0.8;
+
+  will-change: opacity;
+  transition: opacity 600ms;
+
+  ${Wrapper}:hover & {
+    opacity: 1;
+  }
 `
 
 const InnerWrapper = styled.div`
-  color: black;
+  position: absolute;
+  bottom: 0;
+  left: 0;
 
-  &:hover > h2 {
-    color: ${COLORS.bordeaux};
-  }
+  color: black;
+  padding: 16px;
 `
+
+const Title = styled.h1`
+  color: white;
+  filter: drop-shadow(0px 0px 4px hsl(0deg 0% 0% / 1));
+`
+
+const DateDisplay = styled.p``
 
 const PreviewBody = styled.p`
   margin-block-start: 1.25rem;
